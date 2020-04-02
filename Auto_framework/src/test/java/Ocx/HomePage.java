9cbbbba19c1575369efb954f79c896a5001bf62a
Florin <Florin@DESKTOP-ADGURQB>
2020-04-02 15:06:13 +0300

    Fixed browser initialization - Changed from browserName=="xxx" to browserName.equals("xxx"). Also, added a dummy test to check browser initialization

---
+++ Auto_framework/src/test/java/Ocx/HomePage.java
@@ -0,0 +1,17 @@
+package Ocx;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+public class HomePage extends base {
+	
+	@Test
+	public void basePageNavigation() throws IOException
+	{
+		driver=initializeDriver(); 
+		driver.get("https://google.com");
+	}
+	
+
+}
